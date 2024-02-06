4e8c13c5d43f0adaed767e2eb2461951b083a779
Léo DUMONT <114572827+leodumont444@users.noreply.github.com>
2024-02-06 19:28:34 +0100

    feat(include): added blank nanotekspice.hpp

---
+++ include/nanotekspice.hpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** NanoTekSpice.hpp
+*/
+
+#ifndef NANOTEKSPICE_HPP_
+  #define NANOTEKSPICE_HPP_
+
+#endif /* !NANOTEKSPICE_HPP_ */
