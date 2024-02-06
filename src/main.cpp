bcdb75341e2bf441765f0b52ee70db88e9542cda
Léo DUMONT <114572827+leodumont444@users.noreply.github.com>
2024-02-06 19:26:54 +0100

    feat(main): added empty main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** main.cpp
+*/
+
+#include "nanotekspice.hpp"
+
+int main(int argc, const char *argv[], const char *envp[]) {
+    return 0
+}
