4f4f7bcfa034291c47d8ae15aecea6d66ec45964
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-02-13 15:45:21 +0100

    fix(IComponent) change the IComponent

--- src/IComponent.hpp
+++ src/IComponent.hpp
@@ -9,13 +9,25 @@
 
 #include "nanotekspice.hpp"
 
-namespace nts {
+namespace nts
+{
+    enum Tristate {
+        Undefined = (-true),
+        True = true,
+        False = false
+    };
+
     class IComponent {
         public:
             virtual ~IComponent() = default;
-            virtual void simulate() = 0;
+            virtual void simulate(std::size_t tick) = 0;
+            virtual nts::Tristate compute(std::size_t pin) = 0;
+            virtual void setLink(
+                std::size_t pin,
+                nts::IComponent &other,
+                std::size_t otherPin) = 0;
             virtual void dump() const = 0;
+            virtual void setPinValue(std::size_t pin, nts::Tristate value) = 0;
+            virtual nts::Tristate getPinValue(std::size_t pin) const = 0;
     };
 }
-
-
