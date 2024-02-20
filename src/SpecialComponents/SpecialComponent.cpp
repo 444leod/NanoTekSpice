7d0f47e42655703aabae0710de59ca5b3df7e718
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-02-20 18:31:45 +0100

    feat(Special Components): add Special Component parent component

---
+++ src/SpecialComponents/SpecialComponent.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** SpecialComponent
+*/
+
+#include "SpecialComponent.hpp"
+
+SpecialComponent::SpecialComponent(std::string name) : AComponent(name)
+{
+}
+
+SpecialComponent::~SpecialComponent()
+{
+}
---
+++ src/SpecialComponents/SpecialComponent.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** SpecialComponent
+*/
+
+#pragma once
+
+#include "../AComponent.hpp"
+
+class SpecialComponent : public nts::AComponent {
+    public:
+        SpecialComponent(std::string name);
+        ~SpecialComponent();
+
+    protected:
+    private:
+};
