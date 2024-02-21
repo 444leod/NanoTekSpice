7a8ff30c4a12bbcfa6595f36f5851cee1c479d90
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-02-21 03:45:00 +0100

    feat(4001): add 4001 gate

--- src/GatesComponents/My4001.cpp
+++ src/GatesComponents/My4001.cpp
@@ -6,11 +6,49 @@
 */
 
 #include "My4001.hpp"
+#include "../ElementatyComponents/Or.hpp"
+#include "../ElementatyComponents/Not.hpp"
 
-My4001::My4001()
+
+My4001::My4001(std::string name) : GatesComponent(name)
 {
+    _subComponents = {
+        std::make_shared<Or>("Or1"),
+        std::make_shared<Or>("Or2"),
+        std::make_shared<Or>("Or3"),
+        std::make_shared<Or>("Or4"),
+        std::make_shared<Not>("Not1"),
+        std::make_shared<Not>("Not2"),
+        std::make_shared<Not>("Not3"),
+        std::make_shared<Not>("Not4")
+    };
 }
 
 My4001::~My4001()
 {
+}
+
+void My4001::simulate()
+{
+    _subComponents[4]->forceSetLink(_pins[3], 2);
+    _subComponents[5]->forceSetLink(_pins[4], 2);
+    _subComponents[6]->forceSetLink(_pins[10], 2);
+    _subComponents[7]->forceSetLink(_pins[11], 2);
+
+    _subComponents[0]->forceSetLink(_pins[1], 1);
+    _subComponents[0]->forceSetLink(_pins[2], 2);
+    _subComponents[4]->forceSetLink(_subComponents[0]->getPin(3), 1);
+    _subComponents[1]->forceSetLink(_pins[5], 1);
+    _subComponents[1]->forceSetLink(_pins[6], 2);
+    _subComponents[5]->forceSetLink(_subComponents[1]->getPin(3), 1);
+    _subComponents[2]->forceSetLink(_pins[8], 1);
+    _subComponents[2]->forceSetLink(_pins[9], 2);
+    _subComponents[6]->forceSetLink(_subComponents[2]->getPin(3), 1);
+    _subComponents[3]->forceSetLink(_pins[12], 1);
+    _subComponents[3]->forceSetLink(_pins[13], 2);
+    _subComponents[7]->forceSetLink(_subComponents[3]->getPin(3), 1);
+
+    for (auto &subComponent : _subComponents) {
+        subComponent->simulate();
+    }
 }
--- src/GatesComponents/My4001.hpp
+++ src/GatesComponents/My4001.hpp
@@ -11,8 +11,9 @@
 
 class My4001 : public GatesComponent {
     public:
-        My4001();
+        My4001(std::string name);
         ~My4001();
+        void simulate() override;
 
     protected:
     private:
