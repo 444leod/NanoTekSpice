a9a2717bf353cd2389fb316fba80e4abcee38d69
Léo DUMONT <leo.dumont@epitech.eu>
2024-02-29 01:28:26 +0100

    fix(AComponent): changed ACOmponent constructor & added base "simulate" method.

--- src/AComponent.cpp
+++ src/AComponent.cpp
@@ -7,13 +7,10 @@
 
 #include "AComponent.hpp"
 
-int nts::AComponent::nextId = 0;
+using nts::PinType::INPUT;
+using nts::PinType::OUTPUT;
 
-nts::AComponent::AComponent(std::string name)
-{
-    _name = name;
-    _id = nextId++;
-}
+int nts::AComponent::nextId = 0;
 
 nts::AComponent::~AComponent()
 {
@@ -24,9 +21,23 @@ std::string nts::AComponent::getName() const
     return _name;
 }
 
-void nts::AComponent::simulate()
+void nts::AComponent::simulate(std::string currentName)
 {
+    if (std::find(_alreadyTickeds.begin(), _alreadyTickeds.end(), currentName) != _alreadyTickeds.end()) {
+        return;
+    }
+   _alreadyTickeds.push_back(currentName);
+    for (auto &pin : _pins) {
+        if (pin.second != NULL && pin.second->getType() == nts::PinType::INPUT) {
+            pin.second->simulate(currentName);
+        }
+    }
+    subSimulate(currentName);
+}
 
+void nts::AComponent::subSimulate(std::string currentName)
+{
+    (void)currentName;
 }
 
 void nts::AComponent::setPinValue(size_t pin, nts::Tristate value)
@@ -57,7 +68,7 @@ std::shared_ptr<nts::Pin> nts::AComponent::getPin(size_t pin)
 
 void nts::AComponent::setLink(size_t pin, std::shared_ptr<IComponent> other, size_t otherPin)
 {
-    if (_id == other->getId())
+    if (_id == other->getId() && pin == otherPin)
         throw nts::IComponent::LinkException("Cannot link a component to itself");
     if (_pins.find(pin) == _pins.end())
         throw nts::IComponent::LinkException("Pin not found ( linking pin " + std::to_string(pin) + "from " + _name + " to " + other->getName() + " pin " + std::to_string(otherPin));
@@ -65,24 +76,29 @@ void nts::AComponent::setLink(size_t pin, std::shared_ptr<IComponent> other, siz
     std::shared_ptr<Pin> pin1 = _pins[pin];
     std::shared_ptr<Pin> pin2 = other->getPin(otherPin);
 
-    if ((pin1 == NULL && pin2 == NULL))
+    if ((pin1->getType() == INPUT && pin2->getType() == INPUT))
         throw nts::IComponent::LinkException("Both pins are input pins (linking pin " + std::to_string(pin) + " from " + _name + " to " + other->getName() + " pin " + std::to_string(otherPin) + ")");
 
     if ((pin1 && pin1->isIgnored()) || (pin2 && pin2->isIgnored()))
         throw nts::IComponent::LinkException("One of the pins is ignored (linking pin " + std::to_string(pin) + " from " + _name + " to " + other->getName() + " pin " + std::to_string(otherPin) + ")");
 
-    if (pin1 == NULL) {
-        _pins[pin] = pin2;
-    } else if (pin2 == NULL) {
+    if (pin1->getType() == INPUT) {
+        _pins[pin]->setLink(pin2);
+    } else if (pin2->getType() == INPUT){
         other->forceSetLink(_pins[pin], otherPin);
     } else {
         throw nts::IComponent::LinkException("Pins are outputs (linking pin " + std::to_string(pin) + " from " + _name + " to " + other->getName() + " pin " + std::to_string(otherPin) + ")");
     }
 }
 
+void nts::AComponent::setLinkForce(size_t pin, std::shared_ptr<IComponent> other, size_t otherPin)
+{
+    _pins[pin] = other->getPin(otherPin);
+}
+
 void nts::AComponent::forceSetLink(std::shared_ptr<nts::Pin> pin, std::size_t pinIndex)
 {
-    _pins.at(pinIndex) = pin;
+    _pins[pinIndex]->setLink(pin);
 }
 
 bool nts::AComponent::isInput() const
@@ -93,4 +109,16 @@ bool nts::AComponent::isInput() const
 int nts::AComponent::getId() const
 {
     return _id;
+}
+
+void nts::AComponent::linkSubComponents()
+{
+}
+
+void nts::AComponent::dump()
+{
+    for (auto &subComponent : _subComponents) {
+        subComponent.second->dump();
+    }
+    _alreadyTickeds.clear();
 }
