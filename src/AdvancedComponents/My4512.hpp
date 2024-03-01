0d084c7dfebfa3b2d2faee9bad8c8fc5ab2eb841
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-03-01 01:09:44 +0100

    doc(4512): add doc for 4512 class

--- src/AdvancedComponents/My4512.hpp
+++ src/AdvancedComponents/My4512.hpp
@@ -9,6 +9,10 @@
 
 #include "../AComponent.hpp"
 
+/**
+ * @brief My4512 class
+ * @details The 4512 is a 8-channel data selector that can output a Tristate (Undefined, 0, 1)
+*/
 class My4512 : public nts::AComponent {
     public:
         My4512(std::string name);
